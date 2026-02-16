f8643e6a237131bf918181f2db44ab73161a0385
Jose Angel <jaalvarez2818@gmail.com>
2026-02-16 21:40:03 +0100

    Comment out cash reservation logic in test_reserve_cash.py for debugging

--- test_reserve_cash.py
+++ test_reserve_cash.py
@@ -17,29 +17,29 @@ availability_status, availability_data = sdk.search_availability(itinerary=[
     SearchAvailabilityPax(id="4", traveler_type=TravelerType.INFANT),
 ])
 
-if availability_status == 200:
-    pricing_status, pricing_data = sdk.pricing(flight_data=availability_data["data"][0],
-                                               payment_method=PaymentMethod.CASH)
-
-    if pricing_status == 200:
-        reserve_status, reserve_data = sdk.reserve(
-            pricing_data=pricing_data["data"]["flightOffers"][0],
-            payment_method=PaymentMethod.CASH,
-            travelers=[
-                ReservePax(id="1", date_of_birth="1992-12-28", first_name="Jose Angel", last_name="Alvarez Abraira",
-                           gender=Gender.MALE, email="jaalvarez2818@gmail.com", phone_country_code="34",
-                           phone_number="697676672", document_type=DocumentType.PASSPORT, document_number="K111111",
-                           document_issuance_date="2024-05-01", document_expiry_date="2026-05-01",
-                           document_issuance_country_code="CU", nationality_code="CU"),
-                # ReservePax(id="2", date_of_birth="2019-05-18", first_name="Chabelly", last_name="Motes Penna",
-                #            gender=Gender.FEMALE, email="chabellyprueba123@gmail.com", phone_country_code="53",
-                #            phone_number="55555554", document_type=DocumentType.PASSPORT, document_number="K222222",
-                #            document_issuance_date="2024-06-01", document_expiry_date="2026-06-01",
-                #            document_issuance_country_code="ES", nationality_code="ES"),
-                # ReservePax(id="3", date_of_birth="2024-04-22", first_name="Eleany", last_name="Gutierrez Morrell",
-                #            gender=Gender.FEMALE, email="eleanyprueba123@gmail.com", phone_country_code="53",
-                #            phone_number="55555555", document_type=DocumentType.PASSPORT, document_number="K333333",
-                #            document_issuance_date="2024-07-01", document_expiry_date="2026-07-01",
-                #            document_issuance_country_code="US", nationality_code="US"),
-            ]
-        )
+# if availability_status == 200:
+#     pricing_status, pricing_data = sdk.pricing(flight_data=availability_data["data"][0],
+#                                                payment_method=PaymentMethod.CASH)
+#
+#     if pricing_status == 200:
+#         reserve_status, reserve_data = sdk.reserve(
+#             pricing_data=pricing_data["data"]["flightOffers"][0],
+#             payment_method=PaymentMethod.CASH,
+#             travelers=[
+#                 ReservePax(id="1", date_of_birth="1992-12-28", first_name="Jose Angel", last_name="Alvarez Abraira",
+#                            gender=Gender.MALE, email="jaalvarez2818@gmail.com", phone_country_code="34",
+#                            phone_number="697676672", document_type=DocumentType.PASSPORT, document_number="K111111",
+#                            document_issuance_date="2024-05-01", document_expiry_date="2026-05-01",
+#                            document_issuance_country_code="CU", nationality_code="CU"),
+#                 # ReservePax(id="2", date_of_birth="2019-05-18", first_name="Chabelly", last_name="Motes Penna",
+#                 #            gender=Gender.FEMALE, email="chabellyprueba123@gmail.com", phone_country_code="53",
+#                 #            phone_number="55555554", document_type=DocumentType.PASSPORT, document_number="K222222",
+#                 #            document_issuance_date="2024-06-01", document_expiry_date="2026-06-01",
+#                 #            document_issuance_country_code="ES", nationality_code="ES"),
+#                 # ReservePax(id="3", date_of_birth="2024-04-22", first_name="Eleany", last_name="Gutierrez Morrell",
+#                 #            gender=Gender.FEMALE, email="eleanyprueba123@gmail.com", phone_country_code="53",
+#                 #            phone_number="55555555", document_type=DocumentType.PASSPORT, document_number="K333333",
+#                 #            document_issuance_date="2024-07-01", document_expiry_date="2026-07-01",
+#                 #            document_issuance_country_code="US", nationality_code="US"),
+#             ]
+#         )
