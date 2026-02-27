750f8fcca0620779b3be497e46cfd27dafb06ed9
Jose Angel <jaalvarez2818@gmail.com>
2026-02-27 19:11:27 +0100

    Refactor search_availability call in test_reserve_cash.py for improved readability and add only_carriers filter

--- test_reserve_cash.py
+++ test_reserve_cash.py
@@ -6,16 +6,26 @@ from sythonlab_amadeus_enterprise_rest.flights.sdk import FlightSDK
 sdk = FlightSDK(debug=True, prefix_ama_ref="CLT", suffix_ama_ref="user1", currency=Currency.JMD)
 
 availability_status, availability_data = sdk.search_availability(itinerary=[
-    SearchAvailabilityItinerary(id="1", origin_location_code="KIN", destination_location_code="MIA",
-                                departure_date="2026-02-25"),
-    SearchAvailabilityItinerary(id="2", origin_location_code="MIA", destination_location_code="KIN",
-                                departure_date="2026-02-27")
+    SearchAvailabilityItinerary(
+        id="1",
+        origin_location_code="KIN",
+        destination_location_code="MIA",
+        departure_date="2026-02-25"
+    ),
+    SearchAvailabilityItinerary(
+        id="2",
+        origin_location_code="MIA",
+        destination_location_code="KIN",
+        departure_date="2026-02-27"
+    )
 ], travelers=[
     SearchAvailabilityPax(id="1", traveler_type=TravelerType.ADULT),
     SearchAvailabilityPax(id="2", traveler_type=TravelerType.ADULT),
     SearchAvailabilityPax(id="3", traveler_type=TravelerType.CHILD),
     SearchAvailabilityPax(id="4", traveler_type=TravelerType.INFANT),
-])
+], only_carriers=["AV"])
+
+# AVIANCA: AV - IBERIA: IB - AIR FRANCE: AF
 
 if availability_status == 200:
     pricing_status, pricing_data = sdk.pricing(flight_data=availability_data["data"][0],
