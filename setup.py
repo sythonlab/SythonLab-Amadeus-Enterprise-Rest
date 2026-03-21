c9c2d644d8e7293ba3da814520e18821766b7e5f
Jose Angel <jaalvarez2818@gmail.com>
2026-03-21 01:28:39 +0100

    Add setup.py for package configuration and metadata

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sythonlab_amadeus_enterprise_rest",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    url="https://github.com/sythonlab/SythonLab-Amadeus-Enterprise-Rest",
+    author="José Angel Alvarez Abraira",
+    author_email="sythonlab@gmail.com",
+    description="Sython Lab Amadeus Enterprise REST API Client",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
