721b883088f7756d077ba6e8aca60bed3142e1cd
Jose Angel <jaalvarez2818@gmail.com>
2026-06-09 23:54:04 +0200

    Fix end request datetime

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sythonlab_amadeus_enterprise_rest",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(), install_requires=[
         "requests>=2.34.2",
         "python-dotenv>=1.2.2",
